407ea997e9f944d5d44f37cc67a8e7a5c35d10aa
celtare21 <celtare21@gmail.com>
2019-01-18 09:56:47 +0000

    sdm660-common: Optimize ART props
    Change-Id: Ib384409127c09e611d1e875b6827e744298b8499
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

--- device.mk
+++ device.mk
@@ -483,3 +483,12 @@ $(call inherit-product-if-exists, vendor/dolby/config.mk)
 # on supported devices with Deep Press input classifier HALs and models
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.input.video_enabled=false
+
+# ART
+PRODUCT_VENDOR_PROPERTIES += \
+dalvik.vm.dex2oat-filter=speed \
+dalvik.vm.image-dex2oat-filter=speed \
+ro.vendor.qti.am.reschedule_service=true \
+ro.sys.fw.dex2oat_thread_count=8 \
+dalvik.vm.boot-dex2oat-threads=8 \
+dalvik.vm.dex2oat-threads=8
