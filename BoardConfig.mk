44ef034cec3b48db13e608307d5997e2f7532561
Garret Kelly <gdk@google.com>
2019-06-24 11:57:44 -0400

    Set BOOT_SECURITY_PATCH during build
    This property is now used during boot to provide the TEE-backed
    keymaster with the correct boot SPL.  Set it to the platform SPL until
    it diverges as part of an update.
    
    Bug: 119549677
    Test: verified boot.img has property using avbtool
    Change-Id: Ice8e7747059492fc8729128e99119d8678fe5562

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,7 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor security patch level
+BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Inherit from proprietary files
