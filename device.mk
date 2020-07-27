2ba9439432b4127f059167f12abb9e5e6f5da07c
Justin Yun <justinyun@google.com>
2020-07-27 17:20:08 +0900

    sweet: Enforce product interfaces
    Add 'PRODUCT_PRODUCT_VNDK_VERSION := current' and
    'PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true' to enforce
    product interfaces
    
    Change-Id: I25436ecc8b4f2e90ed54b46fd976c66d897348db

--- device.mk
+++ device.mk
@@ -8,6 +8,12 @@
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Enforce native interfaces of product partition as VNDK
+PRODUCT_PRODUCT_VNDK_VERSION := current
+
+# Enforce java interfaces of product partition
+PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
+
 # Product launched with 11.0
 PRODUCT_SHIPPING_API_LEVEL := 30
 
