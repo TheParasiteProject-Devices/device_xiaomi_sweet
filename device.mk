252147b2d328a00bee445e0ab968e4140f6051f1
Shubham Das <dasshubham762@gmail.com>
2021-01-23 15:11:31 +0530

    sweet: Add Camera Go
     * Camera Go is way better than Camera2
    
    Change-Id: If5229936dd4a374256c2474eb0da07d6bad3771f

--- device.mk
+++ device.mk
@@ -71,6 +71,13 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor:64 \
     libpiex_shim
 
+PRODUCT_PACKAGES += \
+    libmm-qcamera \
+    libgui_vendor \
+    libxml2 \
+    CameraGo \
+    vendor.qti.hardware.camera.device@1.0.vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
