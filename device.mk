31c317f11f256fa02870b1cc698cc846ecfb2f9e
Arian <arian.kulmer@web.de>
2021-03-27 17:16:52 +0100

    sm6150-common: livedisplay: Uprev to 2.1
    Change-Id: Icd9d97078cb339aaff04d7e0a52ce8b9b97d35de

--- device.mk
+++ device.mk
@@ -605,7 +605,7 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.xiaomi_sm6150
+    vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
 
 # Power Stats
 PRODUCT_PACKAGES += \
