bd6ce01a62dd7306c97c4adc0443199bf1487110
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-07-15 19:59:31 +0000

    sweet: add Android GO LMKD props
     Added Android GO LMKD Props from go_defaults_common.prop
        - removed statsd
        - Google prefers PSI over vmpressure but in our tests when `ro.lmk.use_psi` was set to `true` and CONFIG_PSI=y [1] , it seems to be acting more aggressive on 2GB Memory devices so removed it (default and current is set to false)
        - `ro.lmk.kill_heaviest_task` is set to `false` by default in Go configuration as well as in LMKD so didn't added it
    
     Rest of lmk props are set automatically when lmkd detects low ram device [`ro.config.low_ram=true`]
     But since we're not actually declaring low_ram so add them separately to override default values
        - `ro.config.per_app_memcg` is set to true when low_ram is declared but we removed it (default and current is set to false)
    
    ref:
    [1]: https://source.android.com/devices/tech/perf/lmkd#using-psi-monitors
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>
    Signed-off-by: ZunayedDihan <zunayeddihan@gmail.com>
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- properties.mk
+++ properties.mk
@@ -267,7 +267,12 @@ PRODUCT_PRODUCT_PROPERTIES += \
 ro.config.low_ram=false \
 ro.lmk.kill_heaviest_task=true \
 ro.lmk.kill_timeout_ms=100 \
-ro.lmk.use_minfree_levels=true
+ro.lmk.use_minfree_levels=true \
+ro.lmk.critical_upgrade=true \
+ro.lmk.upgrade_pressure=40 \
+ro.lmk.downgrade_pressure=60 \
+ro.lmk.thrashing_limit=30 \
+ro.lmk.thrashing_limit_decay=50
 
 # Media
 PRODUCT_VENDOR_PROPERTIES += \
