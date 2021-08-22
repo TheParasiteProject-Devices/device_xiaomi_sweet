fe4960f2c47991f9e835aaa9c891113a53b370b7
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:32:42 +0800

    sdm660-common: Always preopt extracted APKs
    Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -502,3 +502,4 @@ dalvik.vm.minidebuginfo=false
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
