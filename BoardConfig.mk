00cab38006e418157fe2b896f57c0c1b0193abff
LuK1337 <priv.luk@gmail.com>
2021-09-06 11:21:06 +0600

    sm6150-common: Include device FCM from vendor/aosp
    Sadly we aren't able to include it globally without breaking other
    devices ;((((
    
    Change-Id: Ia29d6fb047a09a2b4767312cb2a3252c87095723

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,7 +79,8 @@ KERNEL_SUPPORTS_LLVM_TOOLS := true
 # Manifest
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/manifests/device_framework_matrix.xml \
-    $(DEVICE_PATH)/manifests/lineage_device_framework_matrix.xml
+    $(DEVICE_PATH)/manifests/lineage_device_framework_matrix.xml \
+    vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifests/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/manifests/compatibility_matrix.xml
 ODM_MANIFEST_SKUS := nfc
