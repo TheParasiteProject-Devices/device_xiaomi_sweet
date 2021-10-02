aaf081a156e6788cb7a72cad1fd8cbed6fa78c3f
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2021-10-02 21:05:01 +0530

    sweet: Enable freeform windows

--- device.mk
+++ device.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     libsensorndkbridge
