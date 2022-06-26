f79e0526e98791429f961e601586fe2efd2b33a1
AndreyDenikin1 <andrey.denikin8@gmail.com>
2022-06-26 18:02:59 +0300

    sm6150-common: Import lmkd props from google gki

--- properties.mk
+++ properties.mk
@@ -271,9 +271,11 @@ ro.lmk.use_minfree_levels=true \
 ro.lmk.critical_upgrade=true \
 ro.lmk.upgrade_pressure=40 \
 ro.lmk.downgrade_pressure=60 \
+ro.lmk.filecache_min_kb=153600 \
 ro.lmk.thrashing_limit=30 \
 ro.lmk.thrashing_limit_decay=50 \
-ro.lmk.log_stats=false
+ro.lmk.log_stats=false \
+ro.lmk.stall_limit_critical=40
 
 # Media
 PRODUCT_VENDOR_PROPERTIES += \
