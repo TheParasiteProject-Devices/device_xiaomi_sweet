c9af763e94c23e6a153c06f71d713576e8fcdcc9
someone5678 <nemui3353@gmail.com>
2022-10-13 15:10:20 +0900

    sweet: sepolicy: Import ECOService sepolicies from sunfish

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,6 +155,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor/google
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor/qcom/common
 TARGET_SEPOLICY_DIR := msmsteppe
 -include device/qcom/sepolicy_vndr/SEPolicy.mk
 
