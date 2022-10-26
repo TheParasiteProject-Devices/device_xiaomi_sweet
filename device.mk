7abe0c3a3d77cbb8d2069d0b586dd51a6a5f2a4a
someone5678 <nemui3353@gmail.com>
2022-10-26 21:19:09 +0900

    [DNM] sweet: Drop previous XiaomiParts

--- device.mk
+++ device.mk
@@ -124,10 +124,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
