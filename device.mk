74a4090923e42c51deb515a18316111d4276a634
someone5678 <nemui3353@gmail.com>
2022-10-27 22:28:17 +0900

    sweet: Suppress vintf missing A2DP logspam
    Logcat:
    10-26 22:35:33.028   583   583 I hwservicemanager: getTransport: Cannot find entry android.hardware.bluetooth.a2dp@1.0::IBluetoothAudioOffload/default in either framework or device VINTF manifest.
    10-26 22:35:33.033  6331  6331 E LegacySupport: Could not get passthrough implementation for android.hardware.bluetooth.a2dp@1.0::IBluetoothAudioOffload/default.
    
    * Register to vintf by building mock HAL

--- device.mk
+++ device.mk
@@ -58,6 +58,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-impl.mock \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
