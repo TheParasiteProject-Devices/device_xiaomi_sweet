e2e6dc57b4e44f08b3146fe369c265570fee0741
someone5678 <nemui3353@gmail.com>
2022-10-28 17:11:12 +0900

    sweet: Build more BT packages

--- device.mk
+++ device.mk
@@ -64,6 +64,13 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth.a2dp@1.0-impl.mock \
     android.hardware.bluetooth@1.0.vendor \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    libbtconfigstore \
+    libbthost_if \
+    libbthost_if.vendor \
+    libldacBT_bco.vendor \
+    libldacBT_enc \
+    libldacBT_abr \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
