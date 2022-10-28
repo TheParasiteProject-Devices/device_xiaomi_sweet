f18397a36326706319f8c2f3ab446f20bed8b6d4
someone5678 <nemui3353@gmail.com>
2022-10-28 17:33:25 +0900

    sweet: Uprev android.hardware.bluetooth.audio to 2.1

--- device.mk
+++ device.mk
@@ -61,7 +61,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth.a2dp@1.0-impl.mock \
     android.hardware.bluetooth@1.0.vendor \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
