763432e862bc77944093cdc7ddf7d34de7df594c
someone5678 <nemui3353@gmail.com>
2022-10-31 06:09:03 +0900

    sweet: DEXPREOPT SettingsGoogle and SystemUIGoogle
    * Instead of SystemUI and Settings

--- device.mk
+++ device.mk
@@ -585,8 +585,8 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 WITH_DEXPREOPT_DEBUG_INFO := false
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    Settings \
-    SystemUI
+    SettingsGoogle \
+    SystemUIGoogle
 
 # SystemUI
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
