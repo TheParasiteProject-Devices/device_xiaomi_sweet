48d7c07b28f506af5203e9c6f5286430fb6e392a
someone5678 <nemui3353@gmail.com>
2022-11-08 02:00:23 +0900

    Revert "sweet: Uprev audio hal to 7.1"
    This reverts commit 24703dcce1e299492a2f778c55ff8e9d6d551fe6.

--- device.mk
+++ device.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio@7.1-impl
+    android.hardware.audio@7.0-impl
 
 PRODUCT_PACKAGES += \
     audio.r_submix.default \
