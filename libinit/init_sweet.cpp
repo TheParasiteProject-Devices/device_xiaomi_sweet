17c316ffecf8472e63dca879bfaef29d1ed060b1
someone5678 <nemui3353@gmail.com>
2023-03-31 15:59:34 +0900

    sweet: libinit: Enable ro.sf.blurs_are_expensive

--- libinit/init_sweet.cpp
+++ libinit/init_sweet.cpp
@@ -155,6 +155,7 @@ void vendor_load_properties() {
 
     // Enable UI blur
     property_override("ro.surface_flinger.supports_background_blur", "1");
+    property_override("ro.sf.blurs_are_expensive", "1");
 
     // Set dalvik heap configuration
     std::string heapstartsize, heapgrowthlimit, heapsize, heapminfree,
