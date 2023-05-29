785e265d711d1252773306d8e87371f01e2aa979
someone5678 <nemui3353@gmail.com>
2023-05-29 19:22:00 +0900

    sweet: KernelSU: Clone required repo to build using vendorsetup.sh
    Repos:
    https://github.com/tiann/KernelSU
    https://github.com/TheParasiteProject/kernel_modules

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+# Clone Kernel Modules repo
+if [ ! -d "kernel/modules" ]; then
+    git clone https://github.com/TheParasiteProject/kernel_modules kernel/modules
+fi
+
+# Clone KernelSU repo
+if [ ! -d "kernel/modules/misc/KernelSU" ]; then
+    git clone https://github.com/tiann/KernelSU kernel/modules/misc/KernelSU
+fi
+
+# Update KernelSU repo
+if [ -d "kernel/modules/misc/KernelSU" ]; then
+    cd kernel/modules/misc/KernelSU
+    git reset --hard
+    git fetch origin
+    git pull origin main
+    cd ../../../../
+fi
+
 #
 # Comment out KernelSU's code since kprobes doesn't work properly
 #
 # Ref:
 # https://kernelsu.org/guide/how-to-integrate-for-non-gki.html#integrate-with-kprobe
 #
-sed -i 's/	ksu_enable_sucompat();/	\/\/ksu_enable_sucompat();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
-sed -i 's/	ksu_enable_ksud();/	\/\/ksu_enable_ksud();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
+if [ -f "kernel/modules/misc/KernelSU/kernel/ksu.c" ]; then
+    sed -i 's/	ksu_enable_sucompat();/	\/\/ksu_enable_sucompat();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
+    sed -i 's/	ksu_enable_ksud();/	\/\/ksu_enable_ksud();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
+fi
