116d0552c5b9ed89a9aeb8a57d34dbf7cb19a6ab
someone5678 <nemui3353@gmail.com>
2023-07-09 11:27:58 +0900

    sweet: BoardConfig: Include google specific sepolicies
    * Into SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS
    
    Change-Id: Iaf04f0c26a648fc82ccbb155ac67c0df394094cf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,6 +169,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private/google
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor/google
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor/qcom/common
