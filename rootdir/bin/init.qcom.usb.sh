c55f8f546239c4e62ee6af0ead99db45baab34e2
someone5678 <someone5678@users.noreply.github.com>
2023-09-29 02:35:10 +0900

    sweet: rootdir: init.qcom.usb.sh: Don't set usb string
    * We already set this by libinit
    
    Change-Id: I97681bd4079e17603e54cfb495ac5e6c149097e1

--- rootdir/bin/init.qcom.usb.sh
+++ rootdir/bin/init.qcom.usb.sh
@@ -257,14 +257,6 @@ if [ -d /config/usb_gadget ]; then
 	setprop sys.usb.configfs 1
 fi
 
-# update product
-marketname=`getprop ro.product.marketname`
-if [ "$marketname" != "" ]; then
-    setprop vendor.usb.product_string "$marketname"
-else
-    setprop vendor.usb.product_string "$(getprop ro.product.model)"
-fi
-
 #
 # Initialize RNDIS Diag option. If unset, set it to 'none'.
 #
