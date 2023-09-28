2aa4d73b439e3cf7b3df905f124342062ea0bfc1
someone5678 <someone5678@users.noreply.github.com>
2023-09-28 20:19:16 +0900

    sweet: Drop redundant lmkd/art optimizations
    * Already set in our vendor repo
    
    Change-Id: I6796523b5198d50f0c2bb3edf556489a52e28d82

--- properties.mk
+++ properties.mk
@@ -309,21 +309,6 @@ MultitouchMinDistance=1px \
 TapInterval=1ms \
 TapSlop=1px
 
-# LMKD
-PRODUCT_PRODUCT_PROPERTIES += \
-ro.config.low_ram=false \
-ro.lmk.kill_heaviest_task=true \
-ro.lmk.kill_timeout_ms=100 \
-ro.lmk.use_minfree_levels=true \
-ro.lmk.critical_upgrade=true \
-ro.lmk.upgrade_pressure=40 \
-ro.lmk.downgrade_pressure=60 \
-ro.lmk.filecache_min_kb=153600 \
-ro.lmk.thrashing_limit=30 \
-ro.lmk.thrashing_limit_decay=50 \
-ro.lmk.log_stats=false \
-ro.lmk.stall_limit_critical=40
-
 # Media
 PRODUCT_VENDOR_PROPERTIES += \
 audio.offload.video=true \
