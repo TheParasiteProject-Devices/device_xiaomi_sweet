3cc456f57e60c4862026918c92b6b6f593afb755
someone5678 <someone5678@users.noreply.github.com>
2023-10-09 13:47:14 +0900

    sweet: Build libprotobuf vendorcompats
    Change-Id: I5d52d5813f5fff3a2d1e2db82f57a559516f752a

--- device.mk
+++ device.mk
@@ -572,3 +572,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.0.vendor
+
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-vendorcompat \
+    libprotobuf-cpp-lite-vendorcompat
