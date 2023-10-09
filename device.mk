9d41f802f9c938ac1aaeeaeff6af9ad577b61d74
someone5678 <someone5678@users.noreply.github.com>
2023-10-09 13:43:40 +0900

    sweet: Build vendor var of android.frameworks.sensorservice@1.0
    Change-Id: I53cfdc2df4425a1fc291d903ada3cd99970dc73a

--- device.mk
+++ device.mk
@@ -438,7 +438,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
