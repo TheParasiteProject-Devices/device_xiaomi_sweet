c1eaf8619685f9d02714129146c682eb64af4d01
someone5678 <someone5678@users.noreply.github.com>
2023-10-09 17:59:04 +0900

    sweet: Build libprotobuf-cpp-full-3.9.1-vendorcompat
    Change-Id: I379654b7cefd5add7c0fda3687a4d2b951dad63c

--- device.mk
+++ device.mk
@@ -576,4 +576,5 @@ PRODUCT_PACKAGES += \
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
+    libprotobuf-cpp-lite-vendorcompat \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
