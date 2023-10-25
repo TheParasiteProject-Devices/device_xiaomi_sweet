0bc5da02f672967efd860a42cb53d764dec0ed1a
someone5678 <someone5678@users.noreply.github.com>
2023-10-26 07:49:53 +0900

    sweet: vibrator: Update
    From:
    https://github.com/LineageOS/android_vendor_qcom_opensource_vibrator/commit/7a7737bbe4d2880822b836e587ffbe6706637038
    
    Change-Id: Ie5d9712237b7919568f0cb801c8f59046d2d5048

--- vibrator/service.cpp
+++ vibrator/service.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define LOG_TAG "vendor.qti.hardware.vibrator.service.sweet"
+#define LOG_TAG "vendor.qti.hardware.vibrator.service"
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
