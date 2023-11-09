273daebc1b932d05b1b81c0558bd3e6796f66aca
someone5678 <someone5678@users.noreply.github.com>
2023-11-09 10:09:36 +0900

    sweet: vendorsetup.sh: Guard KernelSU module patching
    * Prevent patching for the case multiple device trees are present
    * export TARGET_PATCH_KERNELSU=true
      to allow patching
    
    Change-Id: I760d5422ecc5e6ea7b1aa08ea6e7ecc52b26f548

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -40,7 +40,10 @@ fi
 # Ref:
 # https://kernelsu.org/guide/how-to-integrate-for-non-gki.html#integrate-with-kprobe
 #
-if [ -f "kernel/modules/misc/KernelSU/kernel/ksu.c" ]; then
-    sed -i 's/	ksu_enable_sucompat();/	\/\/ksu_enable_sucompat();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
-    sed -i 's/	ksu_enable_ksud();/	\/\/ksu_enable_ksud();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
+if [ "$TARGET_PATCH_KERNELSU" = "true" ];
+then
+    if [ -f "kernel/modules/misc/KernelSU/kernel/ksu.c" ]; then
+        sed -i 's/	ksu_enable_sucompat();/	\/\/ksu_enable_sucompat();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
+        sed -i 's/	ksu_enable_ksud();/	\/\/ksu_enable_ksud();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
+    fi
 fi
