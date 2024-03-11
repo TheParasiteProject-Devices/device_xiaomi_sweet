4ff85abde79554a8ed5236af913635d8e5031cfc
Bruno Martins <bgcngm@gmail.com>
2024-03-11 17:23:49 +0100

    sweet: Migrate to common libqti-perfd-client and power-libperfmgr
    Change-Id: I22dd0baaf1594db1650bccf37b4d167b50135174

--- device.mk
+++ device.mk
@@ -393,7 +393,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
@@ -402,6 +402,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     hardware/nxp
 
