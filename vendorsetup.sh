3fde256ea413a16be02895c4d8bf2d695d357ce4
someone5678 <someone5678@users.noreply.github.com>
2024-03-16 22:30:20 +0900

    sweet: vendorsetup.sh: Move KernelSU and KProfiles setup to vendor/parasite
    Change-Id: I9e4ed4f1a1fe67a245f267cb2c9f52daa27586d3

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,39 +1,5 @@
 #!/bin/bash
 
-# Clone Kernel Modules repo
-if [ ! -d "kernel/modules" ]; then
-    mkdir -p kernel/modules
-    touch kernel/modules/Android.mk
-fi
-
-# Clone KernelSU repo
-if [ ! -d "kernel/modules/misc/KernelSU" ]; then
-    git clone https://github.com/tiann/KernelSU kernel/modules/misc/KernelSU
-fi
-
-# Update KernelSU repo
-if [ -d "kernel/modules/misc/KernelSU" ]; then
-    cd kernel/modules/misc/KernelSU
-    git reset --hard
-    git fetch origin
-    git pull origin main
-    cd ../../../../
-fi
-
-# Clone Kprofiles repo
-if [ ! -d "kernel/modules/misc/Kprofiles" ]; then
-    git clone https://github.com/dakkshesh07/Kprofiles kernel/modules/misc/Kprofiles
-fi
-
-# Update Kprofiles repo
-if [ -d "kernel/modules/misc/Kprofiles" ]; then
-    cd kernel/modules/misc/Kprofiles
-    git reset --hard
-    git fetch origin
-    git pull origin main
-    cd ../../../../
-fi
-
 #
 # Comment out KernelSU's code since kprobes doesn't work properly
 #
