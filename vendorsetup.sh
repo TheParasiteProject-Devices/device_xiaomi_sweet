91345a393ea6b67712cf04472af4fc740b4c80c8
someone5678 <someone5678@users.noreply.github.com>
2024-03-18 13:25:02 +0900

    sweet: vendorsetup.sh: Remove TARGET_PATCH_KERNELSU
    * Just use TARGET_PRODUCT check
    
    Change-Id: I36645532e25664d45747ca129aabc33343731835

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,12 +8,9 @@ if [[ $(echo -n $TARGET_PRODUCT | sed -e 's/^aosp_//g') = "sweet" ]]; then
 # Ref:
 # https://kernelsu.org/guide/how-to-integrate-for-non-gki.html#integrate-with-kprobe
 #
-if [ "$TARGET_PATCH_KERNELSU" = "true" ];
-then
-    if [ -f "kernel/modules/misc/KernelSU/kernel/ksu.c" ]; then
-        sed -i 's/	ksu_enable_sucompat();/	\/\/ksu_enable_sucompat();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
-        sed -i 's/	ksu_enable_ksud();/	\/\/ksu_enable_ksud();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
-    fi
+if [ -f "kernel/modules/misc/KernelSU/kernel/ksu.c" ]; then
+    sed -i 's/	ksu_enable_sucompat();/	\/\/ksu_enable_sucompat();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
+    sed -i 's/	ksu_enable_ksud();/	\/\/ksu_enable_ksud();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
 fi
 
 fi
