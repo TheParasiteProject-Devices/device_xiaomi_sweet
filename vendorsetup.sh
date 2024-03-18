c7737e80a5f870224ae622bcb0448a95db2496b1
someone5678 <someone5678@users.noreply.github.com>
2024-03-18 11:14:48 +0900

    sweet: vendorsetup.sh: Guard it
    Change-Id: I247f972b3a46c5ba88b3945a2db1821b31e1e624

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+if [[ $(echo -n $TARGET_PRODUCT | sed -e 's/^aosp_//g') = "sweet" ]]; then
+
 #
 # Comment out KernelSU's code since kprobes doesn't work properly
 #
@@ -12,4 +14,6 @@ then
         sed -i 's/	ksu_enable_sucompat();/	\/\/ksu_enable_sucompat();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
         sed -i 's/	ksu_enable_ksud();/	\/\/ksu_enable_ksud();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
     fi
+fi
+
 fi
