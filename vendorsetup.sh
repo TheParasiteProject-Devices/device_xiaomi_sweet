2d90bb0ce616180f4b8800ecf687f0a48f1b1cd2
someone5678 <someone5678@users.noreply.github.com>
2024-03-23 20:42:15 +0900

    sweet: vendorsetup.sh: Remove KernelSU patching
    * We can disable it by setting CONFIG_KPROBES to false
    
    Change-Id: I7a076f4a4ce68dc13eb300be8cda06c032c04ea0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,1 @@
 #!/bin/bash
-
-if [[ $(echo -n $TARGET_PRODUCT | sed -e 's/^aosp_//g') = "sweet" ]]; then
-
-#
-# Comment out KernelSU's code since kprobes doesn't work properly
-#
-# Ref:
-# https://kernelsu.org/guide/how-to-integrate-for-non-gki.html#integrate-with-kprobe
-#
-if [ -f "kernel/modules/misc/KernelSU/kernel/ksu.c" ]; then
-    sed -i 's/	ksu_enable_sucompat();/	\/\/ksu_enable_sucompat();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
-    sed -i 's/	ksu_enable_ksud();/	\/\/ksu_enable_ksud();/g' kernel/modules/misc/KernelSU/kernel/ksu.c
-fi
-
-fi
