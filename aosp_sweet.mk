3b8290f2f3d2740e455c918e61c20fb4469f5805
someone5678 <someone5678@users.noreply.github.com>
2024-04-26 21:05:09 +0900

    sweet: Parasitizing
    Change-Id: I7b3e2fe25dfdf2e64feccdb6bfca5f34a1a238ce

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -11,16 +11,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common TheParasiteProject stuffs.
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_INCLUDE_CAMERA_GO := true
+TARGET_SUPPORTS_LILY_EXPERIENCE := true
+TARGET_NOT_SUPPORTS_GOOGLE_BATTERY := true
+MAINLINE_INCLUDE_VIRT_MODULE := false
+TARGET_GBOARD_KEY_HEIGHT := 1.2
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi Note 10 Pro
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := M2101K6G
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-google
 
 BUILD_FINGERPRINT := Redmi/sweet_global/sweet:13/TKQ1.221013.002/V14.0.9.0.TKFMIXM:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
