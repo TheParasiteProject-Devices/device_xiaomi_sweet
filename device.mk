e5af4008960936a9937fa058c7cbec522c6b7592
someone5678 <someone5678@users.noreply.github.com>
2024-06-17 20:06:37 +0900

    sweet: Enable blur
    * We added config for blur
    
    Change-Id: I5948237bad5b955e8784adec1f56ba27ccefff1f

--- device.mk
+++ device.mk
@@ -82,6 +82,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0:64 \
