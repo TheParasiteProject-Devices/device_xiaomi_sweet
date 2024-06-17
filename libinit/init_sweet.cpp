b7774217fb68a5aa7e320b8949ee09cd65f56c7a
someone5678 <someone5678@users.noreply.github.com>
2024-06-17 20:03:04 +0900

    sweet: libinit: Remove dalvik heap configs
    * Moved to system
    
    Change-Id: I74a3bb779cf3e0a6c37607b4229a80c5a7b9d1a9

--- libinit/init_sweet.cpp
+++ libinit/init_sweet.cpp
@@ -27,7 +27,6 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <cstdlib>
 #include <fstream>
 #include <unistd.h>
 #include <vector>
@@ -35,7 +34,6 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-#include <sys/sysinfo.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
@@ -115,46 +113,6 @@ void vendor_load_properties() {
     if (std::strcmp(GetProperty("ro.boot.hwc", "").c_str(), "INDIA") != 0) {
         property_override("ro.boot.product.hardware.sku", "nfc");
     }
-
-    // Set dalvik heap configuration
-    std::string heapstartsize, heapgrowthlimit, heapsize, heapminfree,
-			heapmaxfree, heaptargetutilization;
-
-    struct sysinfo sys;
-    sysinfo(&sys);
-
-    if (sys.totalram > 5072ull * 1024 * 1024) {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.5";
-        heapminfree = "8m";
-        heapmaxfree = "32m";
-    } else if (sys.totalram > 3072ull * 1024 * 1024) {
-        // from - phone-xhdpi-4096-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
-        heaptargetutilization = "0.6";
-        heapminfree = "8m";
-        heapmaxfree = "16m";
-    } else {
-        // from - phone-xhdpi-2048-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
-        heaptargetutilization = "0.75";
-        heapminfree = "512k";
-        heapmaxfree = "8m";
-    }
-
-    property_override("dalvik.vm.heapstartsize", heapstartsize.c_str());
-    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit.c_str());
-    property_override("dalvik.vm.heapsize", heapsize.c_str());
-    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization.c_str());
-    property_override("dalvik.vm.heapminfree", heapminfree.c_str());
-    property_override("dalvik.vm.heapmaxfree", heapmaxfree.c_str());
 
 #ifdef __ANDROID_RECOVERY__
     std::string buildtype = GetProperty("ro.build.type", "userdebug");
